2959f5d9b34705a23dc76c1fb89a9fc022ea9664
Eric <emjyoon@gmail.com>
2022-08-26 20:56:49 -0700

    Run me

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.awt.Desktop;
+import java.net.URI;
+
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		Desktop.getDesktop().browse(new URI("https://www.youtube.com/watch?v=dQw4w9WgXcQ"));
+	}
+
+}
